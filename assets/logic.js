7344ac44906849dfd24548076451f0d5929a935d
Kaleb M <kay@Kalebs-MacBook-Air.local>
2020-01-13 20:07:55 -0700

    fixed api

--- assets/logic.js
+++ assets/logic.js
@@ -1,24 +1,41 @@
 // enter api (firebase) info here
 
-
+var firebaseConfig = {
+    apiKey: "AIzaSyCE8s3Pr90yrVE1yOSGE8hxLzUL2HpdWOY",
+    authDomain: "train-scheduler-544cd.firebaseapp.com",
+    databaseURL: "https://train-scheduler-544cd.firebaseio.com",
+    projectId: "train-scheduler-544cd",
+    storageBucket: "train-scheduler-544cd.appspot.com",
+    messagingSenderId: "746905291381",
+    appId: "1:746905291381:web:92b99262c0b20299eb863c",
+    measurementId: "G-YHGMJWE4P3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 var trainData = firebase.database();
 
 $("#addTrainBtn").on("click",function(){
+    event.preventDefault()
     var trainName = $("#trainNameInput").val().trim();
     var destination = $("#destinationInput").val().trim();
-    var tfirstTrain = moment($("#firstTrainInput").val().trim(),"HH:mm").subtract(10,"years").format("x");
+    var firstTrain = moment($("#firstTrainInput").val().trim(),"HH:mm").subtract(10,"years").format("x");
     var frequency = $("#frequencyInput").val().trim();
 
 
-    console.log(firstTrain);
-    return false;
-
+    console.log(trainName,destination, firstTrain, frequency);
+    trainData.ref().push({
+        trainName: trainName,
+        destination:destination,
+        firstTrain:firstTrain,
+        frequency:frequency
+    })
 })
 
 trainData.ref().on("child_added",function(snapshot){
-    var name = snapshot.val().name;
-    var destination = smapshot.val().destination;
+    var name = snapshot.val().trainName;
+    var destination = snapshot.val().destination;
     var frequency = snapshot.val().frequency;
     var firstTrain = snapshot.val().firstTrain;
         
